2035eb9314470afe0259e66731969ac7c727915d
jamilshehab <jamilshehab>
2025-02-26 23:05:40 +0200

    created a display info method

--- BobRentalCar.py
+++ BobRentalCar.py
@@ -5,7 +5,9 @@ class Vichelle:
         self.year=year
         self.rental_price_per_day=rental_price_per_day
 
- 
+    def displayInfo(self):
+        print(f"this car of Brand {self.brand} with model of {self.model} at year {self.year} and of {self.rental_price_per_day}")
+    
    
 class Car(Vichelle):
     def calculate_rental_cars(self,days):
@@ -13,5 +15,5 @@ class Car(Vichelle):
     def __init__(self, brand, model, year, rental_price_per_day,seating_capacity):
         super().__init__(brand, model, year, rental_price_per_day)
         self.seating_capacity=seating_capacity
-
+    
 

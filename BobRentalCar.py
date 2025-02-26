48debebb49a80f2ad3a7fc10334b28c89a4eea00
jamilshehab <jamilshehab>
2025-02-26 22:56:07 +0200

    created a constructur that inherits the main constructur

--- BobRentalCar.py
+++ BobRentalCar.py
@@ -10,5 +10,8 @@ class Vichelle:
 class Car(Vichelle):
     def calculate_rental_cars(self,days):
         return self.rental_price_per_day + days
+    def __init__(self, brand, model, year, rental_price_per_day,seating_capacity):
+        super().__init__(brand, model, year, rental_price_per_day)
+        self.seating_capacity=seating_capacity
 
 

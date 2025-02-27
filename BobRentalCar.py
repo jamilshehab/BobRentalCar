ca5d263346bec9dbfd32f75c8e2e20f12d59ab0f
jamilshehab <jamilshehab>
2025-02-27 11:13:06 +0200

    declaring an overriding method to a car which is display info

--- BobRentalCar.py
+++ BobRentalCar.py
@@ -1,5 +1,5 @@
 class Vichelle:
-    
+
     def __init__(self,brand,model,year,rental_price_per_day):
         self.brand=brand
         self.model=model
@@ -11,10 +11,15 @@ class Vichelle:
     
    
 class Car(Vichelle):
+
     def calculate_rental_cars(self,days):
         return self.rental_price_per_day + days
+    
     def __init__(self, brand, model, year, rental_price_per_day,seating_capacity):
         super().__init__(brand, model, year, rental_price_per_day)
         self.seating_capacity=seating_capacity
-    
+    #declaring an overriding method 
+    def displayInfo(self):
+        print(f"this car of Brand {self.brand} with model of {self.model} at year {self.year} and of {self.rental_price_per_day}")
+
 

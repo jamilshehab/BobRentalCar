30dee3b007e63f41da4888b9d320e7cb2dc2af1b
jamilshehab <jamilshehab>
2025-02-27 17:34:26 +0200

    doing some potential fixes

--- BobRentalCar.py
+++ BobRentalCar.py
@@ -21,10 +21,11 @@ class Car(Vichelle):
     def calculate_rental_cars(self,days):
         return self.rental_price_per_day + days
     
-    def __init__(self, brand, model, year, rental_price_per_day):
+    def __init__(self, brand, model, year, rental_price_per_day,seating_capacity):
         super().__init__(brand, model, year, rental_price_per_day)
+        self.seating_capacity=seating_capacity
 
-    def displayInfo(self,seating_capacity):
+    def displayInfo(self):
         print(f"this car of Brand {self.brand} with model of {self.model} at year {self.year} and a price of {self.get_rental_price_per_day()} with a {seating_capacity} seat") 
     
 class Bike(Vichelle):
@@ -33,16 +34,16 @@ class Bike(Vichelle):
         super().__init__(brand,model,year,rental_price_per_day)
         self.engine_capacity=engine_capacity
 
-    def displayInfo(self,engine_capacity):
-        print(f"this car of Brand {self.brand} with model of {self.model} at year {self.year} and a price of {self.get_rental_price_per_day()} with a {engine_capacity} cc") 
+    def displayInfo(self):
+        print(f"this car of Brand {self.brand} with model of {self.model} at year {self.year} and a price of {self.get_rental_price_per_day()} with a {self.engine_capacity} cc") 
     
 
-toyota=Car("Toyota","Corolla",2020,50)
-yamaha=Bike("Yamaha","R1",30)
+toyota=Car("Toyota","Corolla",5,2020,50)
+yamaha=Bike("Yamaha","R1",2019,998,30)
 
 #creating a function 
 def show_vicheles_info(vichelle):
     return vichelle
  
-show_vicheles_info(toyota.displayInfo(seating_capacity=5))
-show_vicheles_info(yamaha.displayInfo(engine_capacity=998))
+show_vicheles_info(toyota.displayInfo())
+show_vicheles_info(yamaha.displayInfo())

391de435ec0a7f8c5e31941d5ccf97939d14b79d
jamilshehab <jamilshehab>
2025-02-27 16:18:07 +0200

    fixing some errors and adding seating capacity to car class display info method

--- BobRentalCar.py
+++ BobRentalCar.py
@@ -13,7 +13,7 @@ class Vichelle:
         self.__rental_price_per_day=value
 
     def displayInfo(self):
-        print(f"this car of Brand {self.brand} with model of {self.model} at year {self.year} and a price of {self.get_rental_price_per_day()}")
+        print(f"this car of Brand {self.brand} with model of {self.model} at year {self.year} and a price of {self.get_rental_price_per_day()} ")
     
    
 class Car(Vichelle):
@@ -23,12 +23,15 @@ class Car(Vichelle):
     
     def __init__(self, brand, model, year, rental_price_per_day):
         super().__init__(brand, model, year, rental_price_per_day)
-     
+
+    def displayInfo(self,seating_capacity):
+        print(f"this car of Brand {self.brand} with model of {self.model} at year {self.year} and a price of {self.get_rental_price_per_day()} with a {seating_capacity}") 
     
 class Bike(Vichelle):
-    def __init__(self,brand,model,year,rental_price_per_day): 
-        super().__init__(brand,model,)
-bmw=Car("Toyota Corolla",5,2020,50)
-bmw.displayInfo();
+    def __init__(self,brand,model,year,rental_price_per_day,engine_capacity): 
+        super().__init__(brand,model,year,rental_price_per_day)
+        self.engine_capacity=engine_capacity
+bmw=Car("Toyota","Corolla",2020,50)
+bmw.displayInfo(seating_capacity=5);
 
  
